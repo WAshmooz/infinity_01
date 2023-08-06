93faddf4005709437b9e20786604dd15403228ab
WAshmooz <wsa.programming@gmail.com>
2023-08-06 21:06:00 +0300

    dhcp print tree

--- Wes/src/dhcp.c
+++ Wes/src/dhcp.c
@@ -6,7 +6,10 @@
 *******************************************************************************/
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "../include/dhcp.h" 
+#include "../include/aux_funcs.h"
+#include "../include/vector.h"
 
 
 #define BLOCK_SIZE 2
@@ -44,7 +47,7 @@ dhcp_t *DhcpCreate(ip_adr_t address, size_t subnet_size)
 	size_t i = 0;
 	const size_t remaining_bits = 32 - subnet_size;
 	dhcp_t *dhcp;
-	dhcp_node_t *current = {0};
+	dhcp_node_t *current = { 0 };
 	
 	/* Check if subnet_size is within valid range (1 to 32 bits) */
 	if (subnet_size == 0 || subnet_size > 32)
@@ -168,8 +171,121 @@ Notes:			 	Undefined behaviour if "dhcp" is invalid pointer.
 size_t DhcpCountFree(const dhcp_t *dhcp);
 
 
-int main ()
+
+static void PrintNode(const void *element)
 {
+	const dhcp_node_t **elem = (const dhcp_node_t **)element;
+	
+	if (*elem)
+		printf("%2d", (*elem)->is_full);
+	else
+		printf("  ");
+}
+
+static void PrintBTreeFromVector(vector_t *vec, PrintElement printFunc)
+{
+	int i = 0, j = 0, level = 0;
+	int curIdx = 0;
+	int nodes_on_level = 1;
+	void *curNode;
+	int safeStop = 100;
+	int treeSpaces1[] = {
+		126, 0,
+		62,  126,
+		30, 62,
+		14, 30,
+		6, 14,
+		2,  6,
+		0,  2
+	};
+	int treeSpaces2[] = {
+		62,  0,
+		30, 62,
+		14, 30,
+		6, 14,
+		2,  6,
+		0,  2
+	};
+	
+	int *treeSpaces = treeSpaces2;
+	int spacesBefore, spacesInBetween;
+	size_t vectorSize = VectorSize(vec);
+	
+	while (--safeStop && curIdx < vectorSize)
+	{
+		spacesBefore = treeSpaces[level++];
+		spacesInBetween = treeSpaces[level++];
+		printf("%*s", spacesBefore, "");
+		
+		for (i = 0; i < nodes_on_level && curIdx < vectorSize; ++i, ++curIdx)
+		{
+			curNode = VectorGetAccess(vec, curIdx);
+			printFunc(curNode);
+			
+			if (i % 2) {
+				printf("%*s", spacesInBetween, "");
+			}
+			else {
+				for (j = 0; j < spacesInBetween; ++j) printf("-");
+				/* printf("%-s", half-4, ""); */
+			}
+		}
+		nodes_on_level *= 2;
+		printf("\n");
+	}
+	printf("\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+}
+static void TreeToVector(vector_t *vec, dhcp_node_t *root)
+{
+	size_t i;
+	size_t vecSize = VectorSize(vec);
+	dhcp_node_t **elem = (dhcp_node_t**)VectorGetAccess(vec, 0);
+	dhcp_node_t **elem2;
+	
+	*elem = root;
+	
+	printf("%u %p\n", vecSize, *elem);
+	
+	for (i = 0; i < vecSize / 2 - 1; ++i) {
+		elem = (dhcp_node_t**)VectorGetAccess(vec, i);
+		
+		printf("%d %p\n", i, *elem);
+		elem2 = (dhcp_node_t**)VectorGetAccess(vec, 2 * i + 1);
+		if (*elem) *elem2 = (*elem)->node[0];
+		else       *elem2 = NULL;
+		
+		elem2 = (dhcp_node_t**)VectorGetAccess(vec, 2 * i + 2);
+		if (*elem) *elem2 = (*elem)->node[1];
+		else       *elem2 = NULL;
+	}
+}
+static void PrintTree(dhcp_t *dh, vector_t *vec)
+{
+	TreeToVector(vec, dh->root);
+	PrintBTreeFromVector(vec, PrintNode);
+}
+
+
+/* this should pow of 2, because the vector will store tree nodes pointers 
+* so it can be 64, 128, 256 
+*/
+#define VECTROR_CAPACITY 128
+int main()
+{
+	ip_adr_t address = { 0xaa, 0x88, 0xFF, 0x00 };
+	dhcp_t *dh = DhcpCreate(address, 28);
+	vector_t *vec = VectorCreate(VECTROR_CAPACITY, sizeof(void*));
+	int i = 0;
+	void *Stam = NULL;
+	for (i = 0; i < VECTROR_CAPACITY; ++i) {
+		VectorPushBack(vec, &Stam);
+	}
+	
+	PrintTree(dh, vec);
+	
+	printf("End\n");
+	
+	
 	return 0;
 }
 
